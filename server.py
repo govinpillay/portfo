444b4139bdda0153090cfbd73ce9936ed37eff90
govin.pillay <devashreeP25>
2022-10-17 15:51:05 +0200

    initial commit

---
+++ server.py
@@ -0,0 +1,52 @@
+import csv
+import os
+
+from flask import Flask, render_template, request, redirect, send_from_directory
+
+app = Flask(__name__)
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
+@app.route("/")
+def my_home():
+    return render_template('index.html')
+
+
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(page_name)
+
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database.write(f'{email},{subject},{message}\n')
+
+
+def write_to_csv(data):
+    with open('database.csv', newline='', mode='a') as csvfile:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('thankyou.html')
+        except:
+            return 'did not save to database'
+    else:
+        return 'something went wrong. Try again!'
